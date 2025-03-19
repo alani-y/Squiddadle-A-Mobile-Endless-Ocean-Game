ae9fac602171eb5bb4c77e52f4408e706d9e6e90
Alani <alani.yu79@gmail.com>
2025-03-19 14:09:08 -0500

    "started adding shark movement functionality"

--- Assets/Scripts/sharkScript.cs
+++ Assets/Scripts/sharkScript.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class sharkScript : MonoBehaviour
 {
+    private new Rigidbody2D rigidbody2D;
+    public float sharkSpeed = 5;
+    public float xDirection;
+    public float yDirection;
+    public float timer;
+    public float directionInterval = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 newPosition = new Vector3(UnityEngine.Random.Range(-1, 1) * sharkSpeed, UnityEngine.Random.Range(-1, 1)*sharkSpeed);
+        transform.position = newPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= directionInterval){
+            //Vector2 newPosition = transform.position;
+            Vector2 newPosition = new Vector3(UnityEngine.Random.Range(-1f, 1f) * sharkSpeed, UnityEngine.Random.Range(-1f, 1f)*sharkSpeed);
+            transform.position = newPosition;
+            timer = 0;
+        }
     }
 }
