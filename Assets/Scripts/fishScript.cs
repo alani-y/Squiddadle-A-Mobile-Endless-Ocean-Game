12ee48c76e2452f849733bcdc9dc3cfa0665dd18
Alani <alani.yu79@gmail.com>
2025-03-31 22:07:54 -0500

    "corrected inheritance so shark inherits from fish"

--- Assets/Scripts/fishScript.cs
+++ Assets/Scripts/fishScript.cs
@@ -2,30 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fishScript : sharkScript // extends from the shark
+public class fishScript : MonoBehaviour // extends from the fish
 {
-    // Start is called before the first frame update
 
-    // counts how long a fish has existed for
-    private float fishExistenceTimer = 0;
+    private new Rigidbody2D rigidbody2D;
+    public virtual float fishSpeed => 1f;
+    public float directionInterval = 0.5f;
+    public virtual Vector2 movementBounds => new Vector2(-800f, 800f); // keeps the fish's movements to a limited area
 
-    // the fish's initial direction vector
-    public override void Start()
+    // the maximum distances in world units a fish can spawn away from the player
+    public virtual float xMaxBounds => 5;
+    public virtual float yMaxBounds => 2;
+
+    private Vector2 targetPosition;
+    private float timer;
+
+    public virtual void Start()
     {
-        base.Start();
+        targetPosition = GetInitialSpawn();
+        transform.position = targetPosition;
     }
 
-    // Update is called once per frame
-    public override void Update()
+    public virtual void Update()
     {
-        // gives it the same
-        base.Update();
-        fishExistenceTimer += Time.deltaTime;
-        // destroys the fish if its been on the screen for too long
-        if(fishExistenceTimer > 1000){
-            Destroy(gameObject);
+        timer += Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * fishSpeed);
+
+        // checks if the fish arrives at its destination
+        if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            targetPosition = GetRandomPosition();
         }
     }
 
+    public virtual Vector2 GetRandomPosition()
+    {
+        // gets a random vector within the movement bounds of the player
+        return new Vector2(
+            UnityEngine.Random.Range(-movementBounds.x + transform.position.x, movementBounds.x + transform.position.x),
+            UnityEngine.Random.Range(-movementBounds.y + transform.position.y, movementBounds.y + transform.position.y)
+        );
+    }
+
+    public virtual Vector2 GetInitialSpawn(){
+        // x and y components of the fish's final spawn vector
+        float xSpawnVector;
+        float ySpawnVector;
+
+        float rangeSection = UnityEngine.Random.Range(0f, 1.0f);
+
+        // the size of half of the camera view
+        float camHalfHeight = Camera.main.orthographicSize;
+        float camHalfWidth = camHalfHeight * Camera.main.aspect;
+        Debug.Log(camHalfHeight +", " + camHalfWidth);
+
+        // calculates how close a fish can spawn to the player
+        // a fish will always spawn outside of the player's camera view
+        float buffer = 5;
+        float leftMinBounds = transform.position.x - camHalfWidth - buffer;
+        float rightMinBounds = transform.position.x + camHalfWidth + buffer;
+        float topMinBounds = transform.position.y - camHalfHeight - buffer;
+        float bottomMinBounds = transform.position.y + camHalfHeight + buffer;
+
+        // randomly selects if the fish will spawn northwest or southeast of the player
+        if (rangeSection < 0.5){
+            xSpawnVector = UnityEngine.Random.Range(-1*xMaxBounds + transform.position.x, leftMinBounds);
+            ySpawnVector = UnityEngine.Random.Range(-1*yMaxBounds + transform.position.y, topMinBounds);
+        }
+        else{
+            xSpawnVector = UnityEngine.Random.Range(rightMinBounds, transform.position.x + xMaxBounds);
+            ySpawnVector = UnityEngine.Random.Range(bottomMinBounds, transform.position.y + yMaxBounds);
+        }
+
+        return new Vector2(xSpawnVector, ySpawnVector);
+    }
 
 }
--- Assets/Scripts/sharkScript.cs
+++ Assets/Scripts/sharkScript.cs
@@ -5,77 +5,22 @@ using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class sharkScript : MonoBehaviour
+public class sharkScript : fishScript
 {
-    private new Rigidbody2D rigidbody2D;
-    public float sharkSpeed = 1f;
-    public float directionInterval = 0.5f;
-    public Vector2 movementBounds = new Vector2(-800f, 800f); // keeps the shark's movements to a limited area
+    public override float fishSpeed => 4f;
+    public override Vector2 movementBounds => new Vector2(-800f, 800f); // keeps the shark's movements to a limited area
 
-    // the maximum x distance a shark can spawn away from the player
-    public float xMaxBounds = 15;
-    public float yMaxBounds = 5;
-
-    private Vector2 targetPosition;
-    private float timer;
-
-    public virtual void Start()
+    // the maximum distance a shark can spawn away from the player
+    public override float xMaxBounds => 8;
+    public override float yMaxBounds => 3;
+    public override void Start()
     {
-        targetPosition = GetInitialSpawn();
-        transform.position = targetPosition;
+        base.Start();
     }
 
-    public virtual void Update()
+    // Update is called once per frame
+    public override void Update()
     {
-        timer += Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * sharkSpeed);
-
-        // checks if the shark arrives at its destination
-        if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
-        {
-            targetPosition = GetRandomPosition();
-        }
-    }
-
-    public virtual Vector2 GetRandomPosition()
-    {
-        // gets a random vector within the movement bounds of the player
-        return new Vector2(
-            UnityEngine.Random.Range(-movementBounds.x + transform.position.x, movementBounds.x + transform.position.x),
-            UnityEngine.Random.Range(-movementBounds.y + transform.position.y, movementBounds.y + transform.position.y)
-        );
-    }
-
-    public virtual Vector2 GetInitialSpawn(){
-        // x and y components of the shark's final spawn vector
-        float xSpawnVector;
-        float ySpawnVector;
-
-        float rangeSection = UnityEngine.Random.Range(0f, 1.0f);
-
-        // the size of half of the camera view
-        float camHalfHeight = Camera.main.orthographicSize;
-        float camHalfWidth = camHalfHeight * Camera.main.aspect;
-        Debug.Log(camHalfHeight +", " + camHalfWidth);
-
-        // calculates how close a shark can spawn to the player
-        // a shark will always spawn outside of the player's camera view
-        float buffer = 5;
-        float leftMinBounds = transform.position.x - camHalfWidth - buffer;
-        float rightMinBounds = transform.position.x + camHalfWidth + buffer;
-        float topMinBounds = transform.position.y - camHalfHeight - buffer;
-        float bottomMinBounds = transform.position.y + camHalfHeight + buffer;
-
-        // randomly selects if the shark will spawn northwest or southeast of the player
-        if (rangeSection < 0.5){
-            xSpawnVector = UnityEngine.Random.Range(-1*xMaxBounds + transform.position.x, leftMinBounds);
-            ySpawnVector = UnityEngine.Random.Range(-1*yMaxBounds + transform.position.y, topMinBounds);
-        }
-        else{
-            xSpawnVector = UnityEngine.Random.Range(rightMinBounds, transform.position.x + xMaxBounds);
-            ySpawnVector = UnityEngine.Random.Range(bottomMinBounds, transform.position.y + yMaxBounds);
-        }
-
-        return new Vector2(xSpawnVector, ySpawnVector);
+        base.Update();
     }
 }
